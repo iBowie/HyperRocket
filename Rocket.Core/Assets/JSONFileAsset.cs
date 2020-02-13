c04ccd4ffd0ad06b8d2049ebba18bb299e86d54a
iBowie <31174978+iBowie@users.noreply.github.com>
2020-02-13 16:03:59 +0500

    added json file asset

---
+++ Rocket.Core/Assets/JSONFileAsset.cs
@@ -0,0 +1,76 @@
+﻿using Rocket.API;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace Rocket.Core.Assets
+{
+    public class JSONFileAsset<T> : Asset<T> where T : class, IDefaultable
+    {
+        private JsonSerializerSettings settings;
+        private Formatting formatting;
+        private string file;
+        T defaultInstance;
+        public JSONFileAsset(string file, Formatting formatting = Formatting.Indented, JsonSerializerSettings serializerSettings = null, T defaultInstance = null)
+        {
+            this.settings = serializerSettings ?? new JsonSerializerSettings();
+            this.formatting = formatting;
+            this.file = file;
+            this.defaultInstance = defaultInstance;
+            Load();
+        }
+        public JSONFileAsset(string file, Formatting formatting = Formatting.Indented, T defaultInstance = null, params JsonConverter[] converters) : this(file, formatting, new JsonSerializerSettings() { Converters = converters }, defaultInstance)
+        {
+
+        }
+        public override T Save()
+        {
+            try
+            {
+                string directory = Path.GetDirectoryName(file);
+                if (string.IsNullOrEmpty(directory) && !Directory.Exists(directory))
+                    Directory.CreateDirectory(directory);
+                using (StreamWriter writer = new StreamWriter(file))
+                {
+                    if (instance == null)
+                    {
+                        if (defaultInstance == null)
+                        {
+                            instance = Activator.CreateInstance<T>();
+                            instance.LoadDefaults();
+                        }
+                        else
+                        {
+                            instance = defaultInstance;
+                        }
+                    }
+                    string content = JsonConvert.SerializeObject(instance, formatting, settings);
+                    writer.Write(content);
+                    return instance;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Failed to serialize JSONFileAsset: {file}", ex);
+            }
+        }
+        public override void Load(AssetLoaded<T> callback = null)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(file) && File.Exists(file))
+                {
+                    using (StreamReader reader = new StreamReader(file))
+                    {
+                        string content = reader.ReadToEnd();
+                        instance = JsonConvert.DeserializeObject<T>(content, settings);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Failed to deserialize JSONFileAsset: {file}", ex);
+            }
+        }
+    }
+}
---
+++ Rocket.Core/Assets/WebJSONFileAsset.cs
@@ -0,0 +1,77 @@
+﻿using Newtonsoft.Json;
+using Rocket.API;
+using Rocket.Core.Logging;
+using Rocket.Core.Utils;
+using System;
+using System.IO;
+
+namespace Rocket.Core.Assets
+{
+    public class WebJSONFileAsset<T> : Asset<T> where T : class, IDefaultable
+    {
+        private Uri url;
+        private RocketWebClient webclient = new RocketWebClient();
+        private System.Net.DownloadStringCompletedEventHandler handler = new System.Net.DownloadStringCompletedEventHandler((object sender, System.Net.DownloadStringCompletedEventArgs e) => { });
+        private bool waiting = false;
+        public WebJSONFileAsset(Uri url = null, AssetLoaded<T> callback = null)
+        {
+            this.url = url;
+            Load(callback);
+        }
+
+        public override void Load(AssetLoaded<T> callback = null)
+        {
+            try
+            {
+                if (!waiting)
+                {
+                    Logger.Log($"Updating WebJSONFileAsset {typeof(T).Name} from {url}");
+                    waiting = true;
+
+                    webclient.DownloadStringCompleted -= handler;
+                    handler = (object sender, System.Net.DownloadStringCompletedEventArgs e) =>
+                    {
+                        if (e.Error != null)
+                        {
+                            Logger.Log($"Error retrieving WebJSONFileAsset {typeof(T).Name} from {url}: {e.Error.Message}");
+                        }
+                        else
+                        {
+                            try
+                            {
+                                using (StringReader reader = new StringReader(e.Result))
+                                {
+                                    T result = JsonConvert.DeserializeObject<T>(reader.ReadToEnd());
+                                    if (result != null)
+                                    {
+                                        TaskDispatcher.QueueOnMainThread(() =>
+                                        {
+                                            instance = result;
+                                            Logger.Log($"Successfully updated WebJSONFileAsset {typeof(T).Name} from {url}");
+                                        });
+                                    }
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                Logger.Log($"Error retrieving WebJSONFileAsset {typeof(T).Name} from {url}: {ex.Message}");
+                            }
+                        }
+
+                        TaskDispatcher.QueueOnMainThread(() =>
+                        {
+                            callback?.Invoke(this);
+                            waiting = false;
+                        });
+                    };
+                    webclient.DownloadStringCompleted += handler;
+                    webclient.DownloadStringAsync(url);
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Log($"Error retrieving WebJSONFileAsset {typeof(T).Name} from {url}: {ex.Message}");
+            }
+        }
+    }
+}
