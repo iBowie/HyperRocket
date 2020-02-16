5a5f33a9d979a768609fd003b81b4bad28bb48d6
iBowie <31174978+iBowie@users.noreply.github.com>
2020-02-16 20:54:31 +0500

    Update RocketCommandManager.cs

--- Rocket.Core/Commands/RocketCommandManager.cs
+++ Rocket.Core/Commands/RocketCommandManager.cs
@@ -20,7 +20,7 @@ namespace Rocket.Core.Commands
         private readonly List<RegisteredRocketCommand> commands = new List<RegisteredRocketCommand>();
         internal List<RocketCommandCooldown> cooldown = new List<RocketCommandCooldown>();
         public ReadOnlyCollection<RegisteredRocketCommand> Commands { get; internal set; }
-        private XMLFileAsset<RocketCommands> commandMappings;
+        private JSONFileAsset<RocketCommands> commandMappings;
 
         public delegate void ExecuteCommand(IRocketPlayer player, IRocketCommand command, ref bool cancel);
         public event ExecuteCommand OnExecuteCommand;
@@ -34,7 +34,7 @@ namespace Rocket.Core.Commands
         private void Awake()
         {
             Commands = commands.AsReadOnly();
-            commandMappings = new XMLFileAsset<RocketCommands>(Environment.CommandsFile);
+            commandMappings = new JSONFileAsset<RocketCommands>(Environment.CommandsFile);
             checkCommandMappings();
             R.Plugins.OnPluginsLoaded += Plugins_OnPluginsLoaded;
         }
